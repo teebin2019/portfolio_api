1d0f474acfed5cb6d8110717ebac5f242ab53f07
teebin2019 <teebin2019@gmail.com>
2025-10-06 21:21:19 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const usersRouter = require("./routes/users");
+
+const app = express();
+const port = 8080;
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve static files from a directory
+app.use(express.static("public"));
+
+// Use the routers
+app.use("/api/users", usersRouter);
+
+// Define a route for GET requests to the root URL
+app.get("/", (req, res) => {
+  res.send("Hello World from Express!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
