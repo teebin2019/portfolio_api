ab82d98764031ed1cd81a027562e4d30cf743080
teebin2019 <teebin2019@gmail.com>
2025-10-08 13:26:38 +0700

    update users

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 const express = require("express");
 
 const router = express.Router();
-const { PrismaClient } = require("../generated/prisma");
-const prisma = new PrismaClient();
 
 // Middleware specific to this router
 router.use((req, res, next) => {
